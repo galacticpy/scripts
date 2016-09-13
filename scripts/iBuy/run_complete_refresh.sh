a8b25423ec850ebeb767522ec80c43ffbc5cc317
galacticpy <dkenned23@gmail.com>
2016-09-13 13:10:10 -0500

    Update

--- scripts/iBuy/run_complete_refresh.sh
+++ scripts/iBuy/run_complete_refresh.sh
@@ -1,8 +1,4 @@
-#!/bin/sh
-#
-# Copyright (c) 2006 Endeca Technologies Inc. All rights reserved.
-# COMPANY CONFIDENTIAL
-#
+
 
 WORKING_DIR=`dirname ${0} 2>/dev/null`
 . "${WORKING_DIR}/../config/script/set_environment.sh"
